f385123f2348c8866ee0fa396fccd80b581d7af4
Yvann Boucher <yvann.boucher@gmail.com>
2023-11-08 10:35:45 +0400

    MariaDB Connector: Throw error on connection failure

--- .pnp.cjs
+++ .pnp.cjs
@@ -1746,7 +1746,7 @@ const RAW_RUNTIME_STATE =
           ["@tsconfig/node18", "npm:18.2.2"],\
           ["@types/cli-progress", "npm:3.11.5"],\
           ["@types/graphql", null],\
-          ["@types/node", "npm:18.18.8"],\
+          ["@types/node", "npm:18.18.9"],\
           ["cli-progress", "npm:3.12.0"],\
           ["dataloader", "npm:2.2.2"],\
           ["dependency-graph", "npm:0.11.0"],\
@@ -1780,7 +1780,7 @@ const RAW_RUNTIME_STATE =
           ["@swc/jest", "virtual:6aa5de53097a6ad89dd6f5bb86e7514117f570cb46a75f75d77cb5990bf91bfadeda913e418fca44e97d5e167ac1fd89ff92fff8bd06a4e0277076b42b3c17dd#npm:0.2.29"],\
           ["@tsconfig/node18", "npm:18.2.2"],\
           ["@types/cli-progress", "npm:3.11.5"],\
-          ["@types/node", "npm:18.18.8"],\
+          ["@types/node", "npm:18.18.9"],\
           ["cli-progress", "npm:3.12.0"],\
           ["dataloader", "npm:2.2.2"],\
           ["dependency-graph", "npm:0.11.0"],\
@@ -1814,8 +1814,8 @@ const RAW_RUNTIME_STATE =
           ["@swc/jest", "virtual:6aa5de53097a6ad89dd6f5bb86e7514117f570cb46a75f75d77cb5990bf91bfadeda913e418fca44e97d5e167ac1fd89ff92fff8bd06a4e0277076b42b3c17dd#npm:0.2.29"],\
           ["@tsconfig/node18", "npm:18.2.2"],\
           ["@types/graphql", null],\
-          ["@types/node", "npm:18.18.8"],\
-          ["@types/semver", "npm:7.5.4"],\
+          ["@types/node", "npm:18.18.9"],\
+          ["@types/semver", "npm:7.5.5"],\
           ["graphql", "npm:16.8.1"],\
           ["inflection", "npm:3.0.0"],\
           ["jest", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:29.7.0"],\
@@ -1845,8 +1845,8 @@ const RAW_RUNTIME_STATE =
           ["@swc/core", "virtual:6aa5de53097a6ad89dd6f5bb86e7514117f570cb46a75f75d77cb5990bf91bfadeda913e418fca44e97d5e167ac1fd89ff92fff8bd06a4e0277076b42b3c17dd#npm:1.3.96"],\
           ["@swc/jest", "virtual:6aa5de53097a6ad89dd6f5bb86e7514117f570cb46a75f75d77cb5990bf91bfadeda913e418fca44e97d5e167ac1fd89ff92fff8bd06a4e0277076b42b3c17dd#npm:0.2.29"],\
           ["@tsconfig/node18", "npm:18.2.2"],\
-          ["@types/node", "npm:18.18.8"],\
-          ["@types/semver", "npm:7.5.4"],\
+          ["@types/node", "npm:18.18.9"],\
+          ["@types/semver", "npm:7.5.5"],\
           ["graphql", "npm:16.8.1"],\
           ["inflection", "npm:3.0.0"],\
           ["jest", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:29.7.0"],\
@@ -1874,8 +1874,8 @@ const RAW_RUNTIME_STATE =
           ["@types/body-parser", "npm:1.19.5"],\
           ["@types/cors", "npm:2.8.16"],\
           ["@types/express", "npm:4.17.21"],\
-          ["@types/node", "npm:18.18.8"],\
-          ["@types/ws", "npm:8.5.8"],\
+          ["@types/node", "npm:18.18.9"],\
+          ["@types/ws", "npm:8.5.9"],\
           ["body-parser", "npm:1.20.2"],\
           ["cors", "npm:2.8.5"],\
           ["express", "npm:4.18.2"],\
@@ -1901,7 +1901,7 @@ const RAW_RUNTIME_STATE =
           ["@tsconfig/node18", "npm:18.2.2"],\
           ["@types/draft-js", "npm:0.11.15"],\
           ["@types/graphql", null],\
-          ["@types/node", "npm:18.18.8"],\
+          ["@types/node", "npm:18.18.9"],\
           ["entities", "npm:4.5.0"],\
           ["graphql", "npm:16.8.1"],\
           ["jest", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:29.7.0"],\
@@ -1925,7 +1925,7 @@ const RAW_RUNTIME_STATE =
           ["@swc/jest", "virtual:6aa5de53097a6ad89dd6f5bb86e7514117f570cb46a75f75d77cb5990bf91bfadeda913e418fca44e97d5e167ac1fd89ff92fff8bd06a4e0277076b42b3c17dd#npm:0.2.29"],\
           ["@tsconfig/node18", "npm:18.2.2"],\
           ["@types/draft-js", "npm:0.11.15"],\
-          ["@types/node", "npm:18.18.8"],\
+          ["@types/node", "npm:18.18.9"],\
           ["entities", "npm:4.5.0"],\
           ["graphql", "npm:16.8.1"],\
           ["jest", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:29.7.0"],\
@@ -1948,7 +1948,7 @@ const RAW_RUNTIME_STATE =
           ["@tsconfig/node18", "npm:18.2.2"],\
           ["@types/graphql", null],\
           ["@types/indefinite", "npm:2.3.4"],\
-          ["@types/node", "npm:18.18.8"],\
+          ["@types/node", "npm:18.18.9"],\
           ["graphql", "npm:16.8.1"],\
           ["indefinite", "npm:2.4.3"],\
           ["jest", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:29.7.0"],\
@@ -1973,7 +1973,7 @@ const RAW_RUNTIME_STATE =
           ["@swc/jest", "virtual:6aa5de53097a6ad89dd6f5bb86e7514117f570cb46a75f75d77cb5990bf91bfadeda913e418fca44e97d5e167ac1fd89ff92fff8bd06a4e0277076b42b3c17dd#npm:0.2.29"],\
           ["@tsconfig/node18", "npm:18.2.2"],\
           ["@types/indefinite", "npm:2.3.4"],\
-          ["@types/node", "npm:18.18.8"],\
+          ["@types/node", "npm:18.18.9"],\
           ["graphql", "npm:16.8.1"],\
           ["indefinite", "npm:2.4.3"],\
           ["jest", "virtual:dc3fc578bfa5e06182a4d2be39ede0bc5b74940b1ffe0d70c26892ab140a4699787750fba175dc306292e80b4aa2c8c5f68c2a821e69b2c37e360c0dff36ff66#npm:29.7.0"],\
@@ -2601,10 +2601,10 @@ const RAW_RUNTIME_STATE =
         ],\
         "linkType": "HARD"\
       }],\
-      ["npm:18.18.8", {\
-        "packageLocation": "./.yarn/cache/@types-node-npm-18.18.8-5aef4defda-25346cda37.zip/node_modules/@types/node/",\
+      ["npm:18.18.9", {\
+        "packageLocation": "./.yarn/cache/@types-node-npm-18.18.9-db29fe3223-ebd98b117c.zip/node_modules/@types/node/",\
         "packageDependencies": [\
-          ["@types/node", "npm:18.18.8"],\
+          ["@types/node", "npm:18.18.9"],\
           ["undici-types", "npm:5.26.5"]\
         ],\
         "linkType": "HARD"\
@@ -2703,6 +2703,13 @@ const RAW_RUNTIME_STATE =
           ["@types/semver", "npm:7.5.4"]\
         ],\
         "linkType": "HARD"\
+      }],\
+      ["npm:7.5.5", {\
+        "packageLocation": "./.yarn/cache/@types-semver-npm-7.5.5-a3c0668cf4-1b0be2c4d8.zip/node_modules/@types/semver/",\
+        "packageDependencies": [\
+          ["@types/semver", "npm:7.5.5"]\
+        ],\
+        "linkType": "HARD"\
       }]\
     ]],\
     ["@types/send", [\
@@ -2747,10 +2754,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@types/ws", [\
-      ["npm:8.5.8", {\
-        "packageLocation": "./.yarn/cache/@types-ws-npm-8.5.8-6ccd202e33-5f33608d1a.zip/node_modules/@types/ws/",\
+      ["npm:8.5.9", {\
+        "packageLocation": "./.yarn/cache/@types-ws-npm-8.5.9-91d1b2ab07-7cf66383b8.zip/node_modules/@types/ws/",\
         "packageDependencies": [\
-          ["@types/ws", "npm:8.5.8"],\
+          ["@types/ws", "npm:8.5.9"],\
           ["@types/node", "npm:20.8.10"]\
         ],\
         "linkType": "HARD"\
--- packages/graphql-platform-connector-mariadb/src/index.ts
+++ packages/graphql-platform-connector-mariadb/src/index.ts
@@ -150,9 +150,7 @@ export class MariaDBConnector
     this.schema = new Schema(this);
   }
 
-  public getPool(
-    kind: StatementKind = StatementKind.DATA_MANIPULATION,
-  ): mariadb.Pool {
+  public getPool(kind: StatementKind): mariadb.Pool {
     let pool = this.#poolsByStatementKind.get(kind);
     if (!pool) {
       utils.assertPlainObject(this.poolConfig, this.poolConfigPath);
@@ -162,7 +160,9 @@ export class MariaDBConnector
           this.emit(
             'error',
             Object.assign(error, { pool: StatementKind[kind] }),
-          ),
+          ).catch(() => {
+            // Silently ignore the error
+          }),
       };
 
       this.#poolsByStatementKind.set(
@@ -208,11 +208,39 @@ export class MariaDBConnector
     );
   }
 
+  protected async throwOnFatalPoolError(
+    kind: StatementKind,
+    signal: AbortSignal,
+  ): Promise<void> {
+    try {
+      await this.throwOnError(signal);
+    } catch (error) {
+      if (
+        error instanceof mariadb.SqlError &&
+        error.fatal &&
+        (!('pool' in error) || error.pool === StatementKind[kind])
+      ) {
+        throw error;
+      }
+
+      return this.throwOnFatalPoolError(kind, signal);
+    }
+  }
+
   public async withConnection<TResult = unknown>(
     callback: (connection: mariadb.Connection) => Promise<TResult>,
-    kind?: StatementKind,
+    kind: StatementKind = StatementKind.DATA_MANIPULATION,
   ): Promise<TResult> {
-    const connection = await this.getPool(kind).getConnection();
+    const pool = this.getPool(kind);
+
+    // It's a bit tricky, but the client does not throw errors on connection failure like:
+    // -> SqlError: (conn=284, no: 1049, SQLState: 42000) Unknown database '...'
+    const ac = new AbortController();
+    const [, connection] = await Promise.all([
+      this.throwOnFatalPoolError(kind, ac.signal),
+      pool.getConnection().finally(() => ac.abort()),
+    ]);
+
     try {
       return await callback(connection);
     } finally {
